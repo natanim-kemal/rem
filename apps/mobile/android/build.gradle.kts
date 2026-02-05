5f8b8bfcd4af26254e6cb54f5b7cd739faf137fb
natanim-kemal <kemalnatanim@gmail.com>
2026-02-05 12:04:37 +0300

    chore(mobile): update build.gradle.kts

--- apps/mobile/android/build.gradle.kts
+++ apps/mobile/android/build.gradle.kts
@@ -1,7 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+
+    afterEvaluate {
+        tasks.withType(JavaCompile::class.java).configureEach {
+            sourceCompatibility = JavaVersion.VERSION_17.toString()
+            targetCompatibility = JavaVersion.VERSION_17.toString()
+        }
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        extensions.findByName("android")?.let { androidExt ->
+            @Suppress("UNCHECKED_CAST")
+            (androidExt as? com.android.build.api.dsl.CommonExtension<*, *, *, *, *, *>)?.apply {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
     }
 }
 
