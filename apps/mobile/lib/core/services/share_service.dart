8fd93f01b226041d1090590c74de09b935772f9f
natanim-kemal <kemalnatanim@gmail.com>
2026-02-05 12:06:07 +0300

    feat(mobile): update share service

--- apps/mobile/lib/core/services/share_service.dart
+++ apps/mobile/lib/core/services/share_service.dart
@@ -13,34 +13,42 @@ class SharedContent {
 }
 
 class ShareService {
-  final StreamController<SharedContent> _controller = StreamController.broadcast();
+  final StreamController<SharedContent> _controller =
+      StreamController.broadcast();
   Stream<SharedContent> get contentStream => _controller.stream;
   StreamSubscription? _mediaSub;
 
   void initialize() {
-    _mediaSub = ReceiveSharingIntent.instance.getMediaStream().listen((List<SharedMediaFile> value) {
+    _mediaSub = ReceiveSharingIntent.instance.getMediaStream().listen(
+      (List<SharedMediaFile> value) {
+        if (value.isEmpty) return;
+
+        if (value.first.type == SharedMediaType.text ||
+            value.first.type == SharedMediaType.url) {
+          _controller.add(
+            SharedContent(
+              text: value.first.path,
+              type: ShareType.text,
+              files: value,
+            ),
+          );
+          return;
+        }
+
+        _controller.add(SharedContent(files: value, type: ShareType.media));
+      },
+      onError: (err) {
+        debugPrint('getMediaStream error: $err');
+      },
+    );
+
+    ReceiveSharingIntent.instance.getInitialMedia().then((
+      List<SharedMediaFile> value,
+    ) {
       if (value.isEmpty) return;
 
-      if (value.first.type == SharedMediaType.text || value.first.type == SharedMediaType.url) {
-        _controller.add(
-          SharedContent(
-            text: value.first.path,
-            type: ShareType.text,
-            files: value,
-          ),
-        );
-        return;
-      }
-
-      _controller.add(SharedContent(files: value, type: ShareType.media));
-    }, onError: (err) {
-      debugPrint('getMediaStream error: $err');
-    });
-
-    ReceiveSharingIntent.instance.getInitialMedia().then((List<SharedMediaFile> value) {
-      if (value.isEmpty) return;
-
-      if (value.first.type == SharedMediaType.text || value.first.type == SharedMediaType.url) {
+      if (value.first.type == SharedMediaType.text ||
+          value.first.type == SharedMediaType.url) {
         _controller.add(
           SharedContent(
             text: value.first.path,
