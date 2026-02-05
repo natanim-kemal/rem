35c78acff9a63e3bbf6c1d455c0f5d9b83b642e8
natanim-kemal <kemalnatanim@gmail.com>
2026-02-06 01:31:04 +0300

    chore(mobile): remove unused sqlite3 import

--- apps/mobile/lib/data/database/database.dart
+++ apps/mobile/lib/data/database/database.dart
@@ -3,7 +3,6 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
-import 'package:sqlite3/sqlite3.dart';
 
 part 'database.g.dart';
 
